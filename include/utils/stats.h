8bd57deed04faccb02fa34956ad8d62fc9b0e3e1
Kyle Goehner <kgoehner@amazon.com>
2016-12-18 10:20:17 -0500

    Add percetile and max utils

--- include/utils/stats.h
+++ include/utils/stats.h
@@ -1,17 +1,18 @@
 #pragma once
 
 #include <vector>
+#include "utils/stringIt.h"
 
 /**
  * Helper function for determining the mode of a vector.
  * TODO: Should this return a vector?
  */
 template <typename T>
-T getMode(std::vector<T> a) {
-    if (a.empty()) throw std::length_error("Can not find mode of an empty vector.");
-    std::sort(a.begin(), a.end());
+T getMode(std::vector<T> vec) {
+    if (vec.empty()) throw std::length_error("Vector can not be empty.");
+    std::sort(vec.begin(), vec.end());
 
-    auto it = a.cbegin();
+    auto it = vec.cbegin();
 
     size_t bestCount = 1;
     T mode = *it;
@@ -19,7 +20,7 @@ T getMode(std::vector<T> a) {
     size_t count = 1;
     T current = *it;
 
-    for (++it; it != a.cend(); ++it) {
+    for (++it; it != vec.cend(); ++it) {
         if (*it > current) {
             if (count > bestCount) {
                 bestCount = count;
@@ -31,4 +32,26 @@ T getMode(std::vector<T> a) {
         count++;
     }
     return mode;
+}
+
+template <typename T>
+T getMax(std::vector<T> vec) {
+    if (vec.empty()) throw std::length_error("Vector can not be empty.");
+    auto it = vec.begin();
+    T max = *it;
+    for (; it != vec.end(); ++it) {
+        if (*it > max) max = *it;
+    }
+    return max;
+}
+
+template <typename T>
+T getPercentile(std::vector<T> vec, unsigned int p) {
+    if (vec.empty()) throw std::length_error("Vector can not be empty.");
+    if (p > 100U) throw std::invalid_argument(StringIt()
+        << "Percentile can not be greater than 100. Was " << p << ".");
+    if (p == 100U) return getMax(vec);
+    std::sort(vec.begin(), vec.end());
+    const size_t index = p/100.0 * (vec.size()-1) + 0.5f;
+    return vec[index];
 }
--- test/statsTest.cpp
+++ test/statsTest.cpp
@@ -4,13 +4,64 @@
 
 using namespace std;
 
-TEST(statsTest, canGetMode) {
+TEST(statsModeTest, canGetMode) {
     const vector<int> array = {100, 1, 2, 5, 10, 2};
     const int mode = getMode(array);
     EXPECT_EQ(2, mode);
 }
 
-TEST(statsTest, emptyMode) {
+TEST(statsModeTest, emptyMode) {
     const vector<int> array = {};
-    ASSERT_THROW(getMode(array), std::length_error);
+    EXPECT_THROW(getMode(array), std::length_error);
+}
+
+TEST(statsMaxTest, canGetMax) {
+    const vector<int> array = {60, 1, 2, 5, 10, 2};
+    EXPECT_EQ(60, getMax(array));
+}
+
+TEST(statsMaxTest, getMaxThrowWhenEmpty) {
+    const vector<int> array = {};
+    EXPECT_THROW(getMax(array), std::length_error);
+}
+
+TEST(statsPercentileTest, throwWhenOutOfBounds) {
+    const vector<int> array = {60, 1, 2, 5, 10, 2};
+    EXPECT_THROW(getPercentile(array, 101U), std::invalid_argument);
+}
+
+TEST(statsPercentileTest, throwWhenEmpty) {
+    const vector<int> array = {};
+    EXPECT_THROW(getPercentile(array, 0U), std::length_error);
+}
+
+TEST(statsPercentileTest, canGet0Percentile) {
+    const vector<int> array = {60, 1, 2, 5, 10, 2};
+    const int val = getPercentile(array, 0U);
+    EXPECT_EQ(1, val);
+}
+
+TEST(statsPercentileTest, canGet100Percentile) {
+    const vector<int> array = {60, 1, 2, 5, 10, 2};
+    const int val = getPercentile(array, 100U);
+    EXPECT_EQ(60, val);
+}
+
+// We round up to the largest value rather than getting an average.
+TEST(statsPercentileTest, canGet50Percentile) {
+    const vector<int> array = {60, 1, 2, 5, 10, 2};
+    const int val = getPercentile(array, 50U);
+    EXPECT_EQ(5, val);
+}
+
+TEST(statsPercentileTest, canGet25Percentile) {
+    const vector<int> array = {60, -15, 0, 7};
+    const int val = getPercentile(array, 25U);
+    EXPECT_EQ(0, val);
+}
+
+TEST(statsPercentileTest, canGet75Percentile) {
+    const vector<int> array = {60, -15, 0, 7};
+    const int val = getPercentile(array, 75U);
+    EXPECT_EQ(7, val);
 }
